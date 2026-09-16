c0e8ada1493a125c6d80d4785026a473a8998a3a
Nick Lamprecht <nick@lamprecht.net>
2026-09-16 12:42:11 +0200

    fix typo in comment

--- client/src/components/DatetimeField/DatetimeField.js
+++ client/src/components/DatetimeField/DatetimeField.js
@@ -20,7 +20,7 @@ const asHTML5 = (props) => asHTML5DateField(props, hasNativeSupport);
 
 const triggerChange = (props, event, value) => {
   // html5 `datetime-local` input doesn't retain second digits if they're
-  // `00` but that will failed the back-end validation. So add `:00` to the
+  // `00` but that will fail the back-end validation. So add `:00` to the
   // value if they're missing.
   if (/^\d{4}-\d\d-\d\dT\d\d:\d\d$/.test(value)) {
     props.onChange(event, { id: props.id, value: `${value}:00` });
